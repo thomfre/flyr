ea4f0916a11b36dbc09b8cdab07182d4ebefde9c
thomfre <10846906+thomfre@users.noreply.github.com>
2020-12-15 18:18:30 +0100

    Added script skeleton

---
+++ flyr.userscript.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         Flyr
+// @namespace    https://github.com/thomfre/flyr
+// @version      0.1
+// @description  Make yr.no beautiful for pilots
+// @author       You
+// @match        https://www.yr.no/
+// @grant        none
+// ==/UserScript==
+
+(function () {
+  "use strict";
+
+  // Your code here...
+
+})();
