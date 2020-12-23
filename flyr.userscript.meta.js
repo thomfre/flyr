06cf8a12e175ef27ff030cc04c709612d5cda46a
thomfre <10846906+thomfre@users.noreply.github.com>
2020-12-23 09:11:15 +0100

    Create flyr.userscript.meta.js

---
+++ flyr.userscript.meta.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         Flyr
+// @namespace    https://github.com/thomfre/flyr
+// @version      0.3
+// @description  Make yr.no beautiful for pilots
+// @author       thomfre
+// @match        https://www.yr.no/*
+// @grant        GM_getResourceText
+// @grant        unsafeWindow
+// @require      https://raw.githubusercontent.com/mourner/suncalc/master/suncalc.js
+// @resource     airports https://raw.githubusercontent.com/thomfre/flyr/main/airports.json
+// @downloadURL  https://raw.githubusercontent.com/thomfre/flyr/main/flyr.userscript.js
+// @updateURL    https://raw.githubusercontent.com/thomfre/flyr/main/flyr.userscript.meta.js
+// ==/UserScript==
